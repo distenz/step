998f880d7dc867587271bb2281ec7480e9b7960c
Stana Tibor <stana.tibor@creatit.hu>
2022-09-18 19:37:47 +0200

    New node component and graph growth model imp.
    update: Data goes to global store in graph.service.
    update: All network boundaries show in graph component, since nodes
    are undiferentiateable atm, todo created about that. because of this
    we currently see all nodes in top level, and in each node's connection
    tree, if they have paths between them.
    update: Nodes remain expandable, but edge and new node creation
    is migrated to the service.
    remove: GraphEdge component, since this implementation merges
    edge data into the node component itself. the edge data reresentation
    is subject to change (just like everything else)
    
    new: SCSS support, for, well, selector nesting mostly 😊

--- src/step/src/components/GraphNode.vue
+++ src/step/src/components/GraphNode.vue
@@ -1,55 +1,98 @@
 <script setup="" lang="ts">
-import { onMounted, reactive, ref, watch } from 'vue'
-import type { Ref } from 'vue'
-import GraphEdge from './GraphEdge.vue'
+import { graphService, type INodeData } from '@/services/graph.service'
+import { computed, onMounted, reactive, ref } from 'vue'
 
-const store = reactive({
-    id: 0,
+const store = graphService(),
+  props = defineProps<{
+    id: INodeData<any>['id']
+    path: Array<INodeData<any>['id']>
+  }>(),
+  state = reactive({
     input: '',
-    value: '',
-    connections: ref<Array<string>>([]),
+    showConnections: false,
+    data: store.nodes[props.id],
   }),
-  valueElement = ref<Ref<HTMLInputElement> | null>(null),
-  emit = defineEmits(['initialized'])
+  element = ref<HTMLInputElement | undefined>()
 
-defineProps(['edgeIds'])
+const edgeCount = computed(() => state.data.edges.length),
+  connections = computed(() => {
+    return state.data.edges
+      .map(edgeId => {
+        return store.edges[edgeId]
+      })
+      .map(({ fromId, toId }) => {
+        return store.nodes[props.id === fromId ? toId : fromId]
+      })
+      .filter(({ id }) => !props.path.includes(id))
+  })
 
-watch(
-  () => store.value,
-  () => {
-    store.input = ''
-    emit('initialized')
-    if (!store.connections.length)
-      store.connections.push(store.connections.length.toString())
-  }
-)
+onMounted(() => {
+  element.value?.focus()
+})
 
-onMounted(() => valueElement.value?.focus())
-
-function init(input: string) {
-  store.value = input
+function setValue(input: string) {
+  if (!input) return
+  state.data.value = input
 }
 </script>
 
 <template>
-  <article>
-    <p v-if="store.value">Node#{{ store.id }} - {{ store.value }}</p>
-    <input
-      v-else
-      type="text"
-      placeholder="New node value"
-      ref="valueElement"
-      v-model="store.input"
-      @keydown.enter="init(store.input)"
-    />
-    <button v-show="valueElement?.value" @click="init(store.input)">Do</button>
-    <GraphEdge
-      v-for="edgeId in store.connections"
-      :key="edgeId"
-      :edgeId="edgeId"
-      type="child"
-    />
+  <article class="graph__node">
+    <section class="node__data">
+      <span>N#{{ id }} = </span>
+      <p v-if="state.data.value">{{ state.data.value }}</p>
+      <template v-else>
+        <input
+          type="text"
+          ref="element"
+          v-model="state.input"
+          @keyup.enter="setValue(state.input)"
+        />
+        <button @click="setValue(state.input)">Add</button>
+      </template>
+      <p>(E:{{ edgeCount }})</p>
+      <button @click="store.connect(props.id)">Expand</button>
+      <button @click="state.showConnections = !state.showConnections">
+        {{ state.showConnections ? 'Hide Connections' : 'Show Connections' }}
+      </button>
+    </section>
+
+    <section class="node__connections" v-if="state.showConnections">
+      <GraphNode
+        v-for="node in connections"
+        :key="node.id"
+        :id="node.id"
+        :path="[...path, node.id]"
+      />
+    </section>
   </article>
 </template>
 
-<style scroped=""></style>
+<style scoped="" lang="scss">
+.graph__node {
+  background-color: #212121ff;
+  padding: 0.5rem;
+
+  &:not(:last-of-type) {
+    border-bottom: 1px solid #ccc;
+  }
+  .node__data {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+
+    gap: 0.5rem;
+  }
+  p {
+    margin-bottom: 0;
+    line-height: 1.5rem;
+  }
+  input {
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    margin: 0;
+    border: 0;
+  }
+}
+</style>
