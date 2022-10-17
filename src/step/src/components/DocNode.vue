dc8c03a3515b038ea8ee701a4ccfd2a4f796cb48
Stana Tibor <stana.tibor@gmail.com>
2022-10-17 02:35:20 +0200

    WIP: Not nested DOCS representation for a Star

--- src/step/src/components/DocNode.vue
+++ src/step/src/components/DocNode.vue
@@ -1,31 +1,48 @@
 <script setup="" lang="ts">
 import type { IVertex } from '@/utility/vertex.interface'
-import { onMounted, ref, watch, type Ref } from 'vue'
+import { onMounted, onUpdated, nextTick, ref, watch, type Ref } from 'vue'
 
 const props = defineProps<{
-    focused: boolean
+    focused: number
     vertex: IVertex
   }>(),
   emit = defineEmits<{
     (e: 'focus'): void
     (e: 'blur'): void
+    (e: 'up'): void
+    (e: 'down'): void
     (e: 'send', v: IVertex, c: string): void
+    (e: 'remove', v: IVertex): void
   }>(),
   node = ref<HTMLParagraphElement | undefined>(),
-  rows = ref<number>(1)
-
-watch(
-  () => props.focused,
-  focused => {
-    if (focused) focus()
-  }
-)
+  done = ref<boolean>(false)
 
 onMounted(() => {
   if (node.value) {
-    node.value.innerHTML = props.vertex?.value.value ?? ''
+    node.value.innerHTML = props.vertex?.value ?? ''
   }
+  focusIfNeeded()
 })
+onUpdated(() => focusIfNeeded())
+
+function focusIfNeeded() {
+  if (props.focused === props.vertex.id) {
+    focus()
+    if (node.value?.hasChildNodes()) {
+      const selection = window.getSelection()
+      if (selection) {
+        const range = document.createRange()
+        const lastChild: Text = <Text>node.value.lastChild
+        if (lastChild) {
+          range.setStart(lastChild, lastChild.length)
+          range.setEnd(lastChild, lastChild.length)
+          selection.removeAllRanges()
+          selection.addRange(range)
+        }
+      }
+    }
+  }
+}
 
 function focus() {
   node.value?.focus()
@@ -33,31 +50,92 @@ function focus() {
 function send() {
   if (node.value) {
     emit('send', props.vertex, node.value.innerHTML)
-    node.value.innerHTML = ''
   }
+}
+function remove() {
+  if (node.value) {
+    if (node.value.innerText === '') {
+      emit('remove', props.vertex)
+    }
+  }
+}
+function toggle() {
+  done.value = !done.value
+}
+function up() {
+  emit('up')
+}
+function down() {
+  emit('down')
 }
 </script>
 
 <template>
-  <p
-    name="node"
-    type="text"
-    id="node"
-    ref="node"
-    class="host__node"
-    cols="auto"
-    :rows="rows"
-    autofocus
-    autocomplete="off"
-    contentEditable="true"
-    placeholder="Typy type"
-    @focus="$emit('focus')"
-    @blur="$emit('blur')"
-    @keyup.enter="send"
-  ></p>
+  <article>
+    <label :for="`v-${props.vertex.id}`">
+      <input :id="`${props.vertex.id}`" type="checkbox" v-model="done" />
+    </label>
+    <p
+      name="node"
+      type="text"
+      id="node"
+      ref="node"
+      class="host__node"
+      :class="{ 'host__node--done': done }"
+      autocomplete="off"
+      contentEditable="true"
+      placeholder="Typy type"
+      tabindex="0"
+      @focus="$emit('focus')"
+      @blur="$emit('blur')"
+      @keydown.enter.exact.prevent="send"
+      @keydown.delete="remove"
+      @keydown.ctrl.enter.exact.prevent="toggle"
+      @keydown.ctrl.up.exact.prevent="up"
+      @keydown.ctrl.down.exact.prevent="down"
+    ></p>
+  </article>
 </template>
 
 <style scoped="" lang="scss">
+article {
+  display: flex;
+  gap: 0.5ch;
+  align-items: center;
+  justify-content: flex-start;
+
+  font-size: 1.2rem;
+  line-height: 1.4;
+
+  padding-inline: 0.5ch;
+  padding-block: 0.25ch;
+
+  transition: background-color 0.1s linear;
+  border-radius: 0.25ch;
+
+  &:hover {
+    background-color: hsl(0, 0%, 100%, 0.05);
+
+    & > label {
+      opacity: 1;
+    }
+  }
+
+  & > label {
+    display: flex;
+    align-self: flex-start;
+    gap: 0.25ch;
+    opacity: 0;
+
+    transition: opacity 0.1s linear;
+
+    block-size: 1.2rem * 1.4;
+
+    & > input {
+      background-color: transparent;
+    }
+  }
+}
 .host__node {
   inline-size: 100%;
 
@@ -67,8 +145,13 @@ function send() {
   padding: unset;
   margin: unset;
   font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
 
-  font-size: 1.2rem;
-  line-height: 1.5;
+  transition: color 0.1s linear;
+
+  &.host__node--done {
+    color: var(--color-border);
+  }
 }
 </style>
--- src/step/src/views/DocView.vue
+++ src/step/src/views/DocView.vue
@@ -1,56 +1,71 @@
 <script setup lang="ts">
 import { graphService } from '@/services/graph.service'
 import type { IVertex } from '@/utility/vertex.interface'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import DocNode from '../components/DocNode.vue'
 
 const cursor = ref<HTMLTextAreaElement | undefined>(),
-  focused = ref(false),
   store = graphService(),
   parent = ref(store.V[0]),
-  children = ref<Array<IVertex>>(store.heads(parent.value))
+  children = ref<Array<IVertex>>(store.heads(parent.value)),
+  focused = ref<number>(
+    (children.value[children.value.length - 1] || parent.value).id
+  )
 
 onMounted(() => {
   if (children.value.length === 0) {
     next()
   }
 })
-
-function focus() {
-  if (cursor.value && !focused.value) {
-    console.log('next docs cursor focus')
-    focused.value = true
-  }
-}
-function blur() {
-  console.log('next docs cursor blur')
-  focused.value = false
-}
+watch(
+  () => children.value[children.value.length - 1],
+  nextLast => (focused.value = nextLast.id)
+)
 function save(v: IVertex, c: string) {
   console.log('save', v)
-  const vIdx = children.value.findIndex(v => v.id === v.id)
-  children.value[vIdx].value = v.value
+  const vIdx = children.value.findIndex(vf => vf.id === v.id)
+  children.value[vIdx].value = c
 
-  next()
+  if (children.value[children.value.length - 1].value) {
+    next()
+  }
+}
+function remove(v: IVertex) {
+  children.value.splice(
+    children.value.findIndex(fv => fv.id === v.id),
+    1
+  )
+  store.removeV(v.id)
 }
 
 function next() {
   const newV = store.connect(parent.value.id)
+  // focused.value = newV.id
   children.value.push(newV)
+}
+function focusUp() {
+  const fVIdx = children.value.findIndex(fv => fv.id === focused.value)
+  focused.value = children.value[fVIdx - 1]?.id || parent.value.id
+}
+function focusDown() {
+  const fVIdx = children.value.findIndex(fv => fv.id === focused.value)
+  focused.value = children.value[fVIdx + 1]?.id || parent.value.id
 }
 </script>
 
 <template>
-  <main class="host" @click.exact="focus" tabindex="-1">
+  <main class="host" tabindex="-1">
     <DocNode
       v-for="v in children"
       :key="v.id"
       ref="cursor"
       :focused="focused"
       :vertex="v"
-      @focus="focus"
-      @blur="blur"
       @send="save"
+      @remove="remove"
+      @focus="focused = v.id"
+      @up="focusUp"
+      @down="focusDown"
     ></DocNode>
   </main>
 </template>
@@ -66,6 +81,12 @@ function next() {
   display: flex;
   flex-direction: column;
 
-  // background-color: hsla(0, 0, 0, 0.05);
+  justify-content: center;
+
+  // .host__node {
+  //   &:not(:last-of-type) {
+  //     border-bottom: 1px solid var(--color-border);
+  //   }
+  // }
 }
 </style>
